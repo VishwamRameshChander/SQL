2e4af6ef1d6a34fe7bc31d0435cfd5aeedb08328
vishwam <vishwam618@gmail.com>
2017-12-24 10:23:44 +0530

    Completed

--- Consolidation.sql
+++ Consolidation.sql
@@ -21,3 +21,15 @@ UNION ALL
 Select distinct * from disb_dump
 UNION ALL
 Select distinct * from delq_dump;
+
+Create table Nextformula as
+Select distinct Credit_Report.Customer_Name,Credit_Report.LOS_ID, Credit_Report.Surrogate, Credit_Report.State, Credit_Report.Approved_amount, disb_dump.Scheme, disb_dump.Current_Stage FROM Credit_Report
+INNER JOIN disb_dump
+ON Credit_Report.LOS_ID = disb_dump.LOS_ID;
+
+Create table Consolidated as 
+Select distinct Nextformula.LOS_ID, Nextformula.Customer_Name, Nextformula.Current_Stage, Nextformula.Scheme, Nextformula.Surrogate, delq_dump.Bucket, delq_dump.Finance_Amount, delq_dump.Delq_Status, delq_dump.Overdue_Amount from Nextformula
+INNER JOIN delq_dump
+ON Nextformula.LOS_ID = delq_dump.LOS_ID;
+
+
